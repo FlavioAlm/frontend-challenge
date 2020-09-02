82c65533d80ea5861ac79ea0a143999b079441de
flavioalm <flavio.almeida@somoseducacao.com.br>
2020-09-01 21:49:41 -0300

    axios refactoring

--- src/utils/client.js
+++ src/utils/client.js
@@ -2,49 +2,48 @@ import axios from 'axios'
 
 import { getToken } from 'utils'
 
+const PATH_BASE = process.env.REACT_APP_API_URL
+const PATH_SEARCH = '/search'
+const PATH_ARTISTS = '/artists'
+const PATH_ALBUMS = '/albums'
+const PARAM_SEARCH = 'q='
+const PARAM_TYPE= 'type='
+const PARAM_LIMIT= 'limit='
+
+const DEFAULT_TYPE = 'artist'
+const DEFAULT_LIMIT = 10;
+
 class SomosClient {
-  //onError = error => {}
   constructor() {
     this.token = getToken()
+    this.headers = {
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${this.token}`
+      }
+    }
   }
 
-  // axios.get(url, data, config)
-  // ns = async foo(arg) => { func(), return }
   search = async query => {
     const { data } = await axios.get(
-      `${process.env.REACT_APP_API_URL}/search?q=${query}&type=artist`,
-      {
-        headers: {
-          Accept: 'application/json',
-          Authorization: `Bearer ${this.token}`
-        }
-      }, 
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${query}&${PARAM_TYPE}${DEFAULT_TYPE}&${PARAM_LIMIT}${DEFAULT_LIMIT}`,
+      this.headers, 
     )
     return data
   }
 
   getArtist = async id => {
     const { data } = await axios.get(
-      `${process.env.REACT_APP_API_URL}/artists/${id}`,
-      {
-        headers: {
-          Accept: 'application/json',
-          Authorization: `Bearer ${this.token}`
-        }
-      },
+      `${PATH_BASE}${PATH_ARTISTS}/${id}`,
+      this.headers, 
     )
     return data
   }
 
   getArtistAlbums = async id => {
     const { data } = await axios.get(
-      `${process.env.REACT_APP_API_URL}/artists/${id}/albums`,
-      {
-        headers: {
-          Accept: 'application/json',
-          Authorization: `Bearer ${this.token}`
-        }
-      },
+      `${PATH_BASE}${PATH_ARTISTS}/${id}${PATH_ALBUMS}`,
+      this.headers, 
     )
     return data
   }
@@ -53,5 +52,5 @@ class SomosClient {
 
 export default SomosClient
 
-    // Obs: para chamadas na api, você já tem o token salvo no cookie, `authenticated_token` - use ele para mandar no header das chamadas - da uma olhada no `src/utils`
-    // retornar a lista de artistas - https://developer.spotify.com/console/get-several-artists/
+  // Obs: para chamadas na api, você já tem o token salvo no cookie, `authenticated_token` - use ele para mandar no header das chamadas - da uma olhada no `src/utils`
+  // retornar a lista de artistas - https://developer.spotify.com/console/get-several-artists/
